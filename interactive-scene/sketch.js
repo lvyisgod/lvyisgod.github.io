242789c953cec3e7b4d46db57ab845692497f2ee
lvyisgod <caylixx9@outlook.com>
2024-09-25 18:59:44 -0600

    fixed the search function

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,10 +1,11 @@
 // Interactive Scene
 // Demo
+
 let rowLength = 3;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  console.log(searchXandY(0, 1, 5));
+  console.log(searchXandY(1, 0, 2));
 }
 
 function draw() {
@@ -14,27 +15,30 @@ function draw() {
 
 let matrix2d = [
   [0, 2, 0],
-  [0, 5, 0],
+  [0, 0, 3],
   [0, 0, 0]
 ];
 console.log(matrix2d[0][1]);
 
 function searchXandY(colume, row, numToFind){
-  let SearchY = true;
-  for (let times = 1; times < 2; times++){
+  let searchY = true;
+  for (let times = 1; times < 3; times++){
     for (let length = 0; length < rowLength; length++){
-      if (SearchY){
-        if (numToFind === matrix2d[row][length]){
+      if (searchY){
+        console.log(matrix2d[row][length])
+        console.log((matrix2d[row][length] === matrix2d[colume][row]))
+        if ((numToFind === matrix2d[row][length]) && (matrix2d[row][length] === matrix2d[colume][row])){
           return true;
         }
       }
       else {
-        if (numToFind === matrix2d[length][colume]){
+        console.log(matrix2d[length][colume])
+        if ((numToFind === matrix2d[length][colume]) && (matrix2d[length][colume] === matrix2d[colume][row])){
           return true;
         }
       }
     }
-    !SearchY;
+    searchY = false
   }
   return false;
 }
