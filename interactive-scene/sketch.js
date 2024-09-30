b3288f7987b9d5fb7086bd5d05a532b8625bcd82
lvyisgod <caylixx9@outlook.com>
2024-09-30 00:25:32 -0600

    making the game

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,50 +1,61 @@
 // Interactive Scene
 // Demo
 
-let rowLength = 3;
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  makeGame(easymode);
-  console.log(easymode);
-}
-
-function draw() {
-  background(0);
-  circle(mouseX, mouseY, 100);
-}
-
-let easymode = [
+let gameBoard = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0]
 ];
-easymode[2][1] = 2;
+
+let rowLength = 3;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  createGame(gameBoard)
+}
+
+function draw() {
+  background(0);
+  drawGameBoard()
+}
+
+function drawGameBoard(){
+  for (let x = 0; x < windowWidth; x += windowWidth / rowLength) {
+    for (let y = 0; y < windowHeight; y += windowHeight / rowLength) {
+      square(x, y, windowWidth / 2);
+      text(gameBoard[0][1], x+windowWidth/(rowLength*2), y-windowWidth/(rowLength*2))
+    }
+  }
+}
+
 
 function searchXandY(colume, row, numToFind, easyOrHard){
-  let searchY = true;
+  let searchX = true;
   for (let times = 0; times < 2; times++){
     for (let length = 0; length < rowLength; length++){
-      if (searchY){
-        if (numToFind === easyOrHard[row][length] && length !== row){
+      if (searchX){
+        if (numToFind === easyOrHard[row][length] && length !== colume){
           return true;
         }
       }
       else {
-        if (numToFind === easyOrHard[length][colume] && length !== colume){
+        if (numToFind === easyOrHard[length][colume] && length !== row){
           return true;
         }
       }
     }
-    searchY = false;
+    searchX = false;
   }
   return false;
 }
 
-function makeGame(easyOrHard){
-  for (let x = 0; x < 1; x++){
-    for (let y = 0; y < 2; x++){
-      console.log[x, y];
+function createGame(easyOrHard){
+  for (let y = 0; y < rowLength; y++){
+    for (let x = 0; x < rowLength; x++){
+      easyOrHard[y][x] = round(random(1, rowLength))
+      while (searchXandY(x, y, easyOrHard[y][x], easyOrHard)){
+        easyOrHard[y][x] = round(random(1, rowLength))
+     }
     }
   }
 }
