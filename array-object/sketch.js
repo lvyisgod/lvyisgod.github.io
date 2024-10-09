802715a85dc60b87c821529124574dc15c6ced67
lvyisgod <caylixx9@outlook.com>
2024-10-09 14:17:06 -0600

    created the shuffler and Table cards then started on putting cards on the table

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -3,14 +3,25 @@
 // 10/8/2024
 // Extra for experts:
 
-let cardNumbers = ["ACE", 2, 3, 4, 5, 6, 7, 8, 9, 10, "JACK", "QUEEN", "KING"];
-let suits = ["s", "c", "h", "d"];
+const cardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const suits = ["s", "c", "h", "d"];
 let deck = [];
+let cardsOnTable = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  []
+];
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   makeDeck();
-  console.log(deck);
+  suffleDeck();
+
 }
 
 function draw() {
@@ -22,5 +33,15 @@ function makeDeck(){
     for (let cardNum of cardNumbers){
       deck.push(suit + cardNum);
     }
+  }
+}
+
+function suffleDeck(){
+  deck = deck.sort(() => 0.5 - Math.random());
+}
+
+function putCardsOnTable(){
+  for(let cards = 1; cards > 7; card++){
+    
   }
 }
