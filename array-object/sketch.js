70fdd3bd4e779bceffdcb8ab10047ca01bd93204
lvyisgod <caylixx9@outlook.com>
2024-10-11 11:31:03 -0600

    trying to figure out putting cards on the table

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -7,13 +7,13 @@ const cardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 const suits = ["s", "c", "h", "d"];
 let deck = [];
 let cardsOnTable = [
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  []
+  [0],
+  [0, 0],
+  [0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
 ];
 
 
@@ -21,7 +21,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   makeDeck();
   suffleDeck();
-
+  putCardsOnTable();
+  console.log(deck);
+  console.log(cardsOnTable);
 }
 
 function draw() {
@@ -40,8 +42,11 @@ function suffleDeck(){
   deck = deck.sort(() => 0.5 - Math.random());
 }
 
-// function putCardsOnTable(){
-//   for(let cards = 1; cards > 7; card++){
-    
-//   }
-// }
+function putCardsOnTable(){
+  for(let listnum = 0; listnum < cardsOnTable.length - 1; listnum++){
+    for(let card = 0; card < cardsOnTable[listnum].length ; card++){
+      cardsOnTable[listnum].splice(card, card, deck[0]);
+      deck.shift();
+    }
+  }
+}
