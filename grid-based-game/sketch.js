763b3a905eccd073d5ab71f1f74a23808a6d411c
lvyisgod <caylixx9@outlook.com>
2024-10-30 14:17:05 -0600

    made a random game board on the input that was inputted and make a game state startscreen gamescreen and noinput screen

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -6,24 +6,45 @@
 // - describe what you did to take this project "above and beyond"
 
 let gameBoard = [];
-let cellsize;
+let gridsize;
 let input;
 let p = 0;
+let gameState = "startSceen";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  input = createInput('');
-  input.position(0, 100);
+  input = createInput('', 'number');
+  input.position(width/2.5, height / 2);
+  setTimeout(printWO, 2000);
 }
 
 function draw() {
-  if (p === 0){
-    setTimeout(printWO, 3000);
-    p += 1;
-  }
   background(220);
+  if (gameState === "startSceen"){
+    background("green");
+    textSize(50);
+    text("Please type in a number for your grid size you have 10 seconds", 100, height/3);
+  }
+  else if (gameState === "gameScreen"){
+    displayGameBoard();
+  }
+  else if (gameState === "noInputEntered"){
+
+  }
 }
 
 function printWO(){
-  console.log(input.input);
+  gridsize = input.value();
+  removeElements();
+  generateRandomGameBoard();
+  gameState = "gameScreen";
+}
+
+function generateRandomGameBoard(){
+  for (let y = 0; y < gridsize; y++){
+    gameBoard.push([]);
+    for (let x = 0; x < gridsize; x++){
+      gameBoard[y].push(round(random(1)));
+    }
+  }
 }
