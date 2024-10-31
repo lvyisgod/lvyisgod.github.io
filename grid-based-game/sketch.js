bafb8596dd4ef9a403b24dd7cc2ff82761300912
lvyisgod <caylixx9@outlook.com>
2024-10-31 14:16:51 -0600

    made the error screen if the input is larger then 8 or less than 1 or no input and also made it display

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -7,6 +7,7 @@
 
 let gameBoard = [];
 let gridsize;
+let cellSize;
 let input;
 let p = 0;
 let gameState = "startSceen";
@@ -24,20 +25,32 @@ function draw() {
     background("green");
     textSize(50);
     text("Please type in a number for your grid size you have 10 seconds", 100, height/3);
+    textSize(30);
+    text("please input a number that is smaller than 8 and bigger then 1", 300, height/2.5);
   }
   else if (gameState === "gameScreen"){
+    background("white");
     displayGameBoard();
   }
-  else if (gameState === "noInputEntered"){
-
+  else if (gameState === "errorInputEntered"){
+    background("red");
+    textSize(50);
+    text("error number too small or too large or no number inputed", width/8, height/2);
   }
 }
 
 function printWO(){
-  gridsize = input.value();
-  removeElements();
-  generateRandomGameBoard();
-  gameState = "gameScreen";
+  if (input.value() > 1 && input.value() < 8 && input.value() !== ''){
+    gridsize = input.value();
+    cellSize = height/gridsize/1.5;
+    removeElements();
+    generateRandomGameBoard();
+    gameState = "gameScreen";
+  }
+  else{
+    removeElements();
+    gameState = "errorInputEntered";
+  }
 }
 
 function generateRandomGameBoard(){
@@ -45,6 +58,34 @@ function generateRandomGameBoard(){
     gameBoard.push([]);
     for (let x = 0; x < gridsize; x++){
       gameBoard[y].push(round(random(1)));
+    }
+  }
+}
+
+function displayGameBoard(){
+  for (let y = 0; y < gridsize; y++){
+    for (let x = 0; x < gridsize; x++){
+      if (gameBoard[y][x] === 0){
+        fill("red");
+      }
+      else if (gameBoard[y][x] === 1){
+        fill("black");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+function displayGrid(){
+  for(let y = 0; y < GRID_SIZE; y++){
+    for(let x = 0; x < GRID_SIZE; x++){
+      if (grid[y][x] === 1){
+        fill("darkblue");
+      }
+      else{
+        fill("lightpink");
+      }
+      square(x+cellSize * cellSize, y * cellSize, cellSize);
     }
   }
 }
--- grid/sketch.js
+++ grid/sketch.js
@@ -41,7 +41,7 @@ function displayGrid(){
       else{
         fill("lightpink");
       }
-      square(x * cellSize, y* cellSize, cellSize);
+      square(x * cellSize, y* cellSize, cellSize/2);
     }
   }
 }
