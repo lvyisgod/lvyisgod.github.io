fcf00fdb0a6e09977b3e621eba60f748040cc945
lvyisgod <caylixx9@outlook.com>
2024-11-14 13:24:57 -0600

    made a new walker demo

---
+++ walker-oop-array/sketch.js
@@ -0,0 +1,63 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+class Walker{
+  constructor(x, y, theColor){
+    this.x = x;
+    this.y = y;
+    this.radius = 4;
+    this.speed = 8;
+    this.color = theColor;
+  }
+
+  display() {
+    noStroke();
+    fill(this.color);
+    circle(this.x, this.y, this.radius*2);
+  }
+
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+      //up
+      this.y -= this.speed;
+    }
+    else if (choice < 50) {
+      //down
+      this.y += this.speed;
+    }
+    else if (choice < 75) {
+      //left
+      this.x -= this.speed;
+    }
+    else {
+      //right
+      this.x += this.speed;
+    }
+  }
+}
+
+let walkerArray = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  let luc = new Walker(width/2, height/2, "red");
+  walkerArray.push(luc);
+}
+
+function draw() {
+  for (let theWalker of walkerArray){
+    theWalker.move();
+    theWalker.display();
+  }
+}
+
+function mousePressed() {
+  let randomColor = color(random(255), random(255), random(255));
+  let someWalker = new Walker(mouseX, mouseY, randomColor);
+  walkerArray.push(someWalker);
+}
